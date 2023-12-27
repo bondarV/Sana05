c74de77717827bd280075c7c316815b035463d62
VitaliyBondar <ipz234_bvo@student.ztu.edu.ua>
2023-12-27 11:57:15 +0200

    Додані усі класи завдання та логічно представленні методи до них

---
+++ Program/Currency.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sana05
+{
+    public class Currency
+    {
+
+        public string Name { get; set; }
+        public float ExRate {  get; set; }
+
+        public Currency(string name, float exRate)
+        {
+            Name = name;
+            ExRate = exRate;
+        }
+
+        public Currency(string name)
+        {
+            Name = name;
+        }   
+        public Currency()
+        {
+            Name = "USD";
+            ExRate = 37.55F;
+        }
+        public Currency(Currency obj)
+        {
+            Name = obj.Name;
+            ExRate = obj.ExRate;
+        }
+    }
+}
---
+++ Program/Product.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sana05
+{
+    public class Product
+    {
+        public string Name { get; set; }
+
+        public float Price { get; set; }
+        Currency Cost { get; set; }
+        public ushort Quantity { get; set; }
+        public string Producer { get; set; }
+        public float Weight { get; set; }
+
+        public Product(string name, float price, Currency cost, ushort quantity, string producer, float weight)
+        {
+            Name = name;
+            Price = price;
+            Cost = cost;
+            Quantity = quantity;
+            Producer = producer;
+            Weight = weight;
+        }
+        public Product(float price, Currency cost, ushort quantity, string producer, float weight)
+        {
+            Name = "Кавоварка";
+            Price = price;
+            Cost = cost;
+            Quantity = quantity;
+            Producer = producer;
+            Weight = weight;
+        }
+
+        public Product() {
+            Name = "Сертифікат від магазину";
+            Price = 10;
+            Cost = new Currency();
+            Quantity = 1000;
+            Producer = "Shop";
+            Weight = 0;
+
+        }
+
+        public Product(Product obj)
+        {
+            Name = obj.Name;
+            Price = obj.Price;
+            Cost = obj.Cost;
+            Quantity = obj.Quantity;
+            Producer = obj.Producer;
+            Weight = obj.Weight;
+
+        }
+        public float GetPriceInUAH()
+        {
+            if(Cost.Name != "UAH") 
+            return Price * Cost.ExRate;
+            return Price;
+        }
+        public float GetTotalPriceInUAH()
+        {
+            return Price * GetPriceInUAH();
+        }
+
+        public float GetTotalWeight(Product product)
+        {
+            return product.Weight;
+        }
+    }
+}
